9338f5002196b3fa1cece32e1eeef803db8bc400
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-02-25 18:25:45 +0300

    Initial Commit

---
+++ App363/App363App.swift
@@ -0,0 +1,17 @@
+//
+//  App363App.swift
+//  App363
+//
+//  Created by IGOR on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct App363App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
